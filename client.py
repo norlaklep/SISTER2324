abe80d77ce6b4693c9cdc15474c5e7007dba8122
Fajar Maulana Kadir <67577504+norlaklep@users.noreply.github.com>
2023-11-25 08:26:38 +0700

    Update client.py

--- client.py
+++ client.py
@@ -1,11 +1,13 @@
-import socket
+import xmlrpc.client
+
+def login():
+    user_type = input("Enter user type (admin/patient): ")
+    username = input("Enter username: ")
+    return user_type, username
 
 def main():
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.connect(('127.0.0.1', 5555))
-
-    client_name = input("Enter your name: ")
-    client.send(client_name.encode())
+    client = xmlrpc.client.ServerProxy('http://127.0.0.1:5555/')
+    user_type, username = login()
 
     while True:
         print("\nOptions:")
@@ -17,29 +19,43 @@ def main():
         choice = input("Enter your choice (1-4): ")
 
         if choice == "1":
-            client.send("REGISTER".encode())
-            response = client.recv(1024).decode()
-            print(response)
+            if user_type == "patient":
+                if not client.is_patient_registered(username):
+                    response = client.register(username)
+                    if response != -1:
+                        print(f"Registered successfully. Your queue number is {response}")
+                    else:
+                        print("You are already registered.")
+                        remove_queue = input("Do you want to remove your existing queue? (yes/no): ")
+                        if remove_queue.lower() == "yes":
+                            # Implement logic to remove patient's queue
+                            print("Queue removed.")
+                else:
+                    print("You are already registered.")
+
+            elif user_type == "admin":
+                print("Admins cannot register to clinics.")
 
         elif choice == "2":
-            client.send("GET_QUEUE".encode())
-            response = client.recv(1024).decode()
+            response = client.get_queue()
             print(f"Current queue: {response}")
 
         elif choice == "3":
-            client_number = input("Enter your queue number: ")
-            client.send(f"ESTIMATE_WAIT_TIME {client_number}".encode())
-            response = client.recv(1024).decode()
-            print(response)
+            if user_type == "patient":
+                client_number = int(input("Enter your queue number: "))
+                response = client.estimate_wait_time(client_number)
+                if response != -1:
+                    print(f"Your estimated wait time is {response} minutes.")
+                else:
+                    print("Invalid client number.")
+            elif user_type == "admin":
+                print("Admins cannot estimate wait time for patients.")
 
         elif choice == "4":
-            client.send("EXIT".encode())
             break
 
         else:
             print("Invalid choice. Please enter a number between 1 and 4.")
-
-    client.close()
 
 if __name__ == "__main__":
     main()
