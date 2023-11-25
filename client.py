728937900a2ebaa26c2be85c45dece9d207dfd4d
Fajar Maulana Kadir <67577504+norlaklep@users.noreply.github.com>
2023-11-25 08:49:00 +0700

    Update client.py

--- client.py
+++ client.py
@@ -1,9 +1,25 @@
 import xmlrpc.client
+from datetime import datetime
 
 def login():
     user_type = input("Enter user type (admin/patient): ")
     username = input("Enter username: ")
     return user_type, username
+
+def choose_clinic(client):
+    clinics = client.get_clinics()
+    print("Choose a clinic:")
+    for clinic_name, opening_hours in clinics.items():
+        print(f"{clinic_name} : {opening_hours}")
+
+    clinic_name = input("Enter the name of the clinic you want to register to: ")
+    return clinic_name
+
+def get_patient_info():
+    medical_record_number = input("Enter your medical record number (e.g., 12-34-56): ")
+    name = input("Enter your name: ")
+    birthdate = input("Enter your birthdate (YYYY-MM-DD): ")
+    return medical_record_number, name, birthdate
 
 def remove_patient_queue(client, username):
     remove_queue = input("Do you want to remove your existing queue? (yes/no): ")
@@ -32,10 +48,14 @@ def main():
 
         if choice == "1":
             if user_type == "patient":
+                clinic_name = choose_clinic(client)
+
                 if not client.is_patient_registered(username):
-                    response = client.register(username)
+                    response = client.register(username, clinic_name, *get_patient_info())
                     if response != -1:
                         print(f"Registered successfully. Your queue number is {response}")
+                    elif response == -2:
+                        print("The chosen clinic is currently closed. Please choose another time or clinic.")
                     else:
                         print("You are already registered.")
                         remove_patient_queue(client, username)
@@ -59,13 +79,4 @@ def main():
                 else:
                     print("Invalid client number.")
             elif user_type == "admin":
-                print("Admins cannot estimate wait time for patients.")
-
-        elif choice == "4":
-            break
-
-        else:
-            print("Invalid choice. Please enter a number between 1 and 4.")
-
-if __name__ == "__main__":
-    main()
+                print("
